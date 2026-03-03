68147107c6ca9a72e3595035715b10cd0dfc3fd2
Ivan <roma.teshev2013@yandex.ru>
2026-03-03 15:33:50 +0300

    Rebuild function with pd

--- strategy.py
+++ strategy.py
@@ -1,9 +1,8 @@
 # формулы стратегии (mean10, mean8worst, D, сравнение с акциями)
 
 def calculate_dispersion(price_dataframe):
-    return_list = (price_dataframe.iloc[-1] / price_dataframe.iloc[0]) - 1
-    worst_8_returns = sorted(return_list)[0:8]
-    mean_best_10 = sum(return_list)/len(return_list)
-    mean_worst_8 = sum(worst_8_returns)/len(worst_8_returns)
-    res = mean_best_10 - mean_worst_8
-    return res
+    total_returns = (price_dataframe.iloc[-1] / price_dataframe.iloc[0]) - 1
+    portfolio_return = total_returns.mean()
+    worst_8_returns = total_returns.sort_values()[:8].mean()
+    total_spread = portfolio_return - worst_8_returns
+    return total_spread
